fa6c3de8aa8d396c986ba5ed9fcf69393ea724c4
tlMark <127731453+tlMark@users.noreply.github.com>
2025-05-23 00:55:49 +0900

    EnemySpawner 만들고 적용용

--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject _enemyPrefab;
+
+    [SerializeField] private float _minimumSpawnTime;
+
+    [SerializeField] private float _maximumSpawnTime;
+
+    private float _timeUntilSpawn;
+
+    void Awake()
     {
-        
+        SetTimeUntilSpawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _timeUntilSpawn -= Time.deltaTime;
+
+        if (_timeUntilSpawn <= 0)
+        {
+            Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+            SetTimeUntilSpawn();
+        }
+    }
+    
+    private void SetTimeUntilSpawn()
+    {
+        _timeUntilSpawn = Random.Range(_minimumSpawnTime, _maximumSpawnTime);
     }
 }
